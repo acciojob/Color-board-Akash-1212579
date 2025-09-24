c442118810b9158213544a5ff4ddd207cf265720
Akash-1212579 <dalbhanjanakash1@gmail.com>
2025-09-24 23:14:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
  const board = document.getElementById("board");
 
-    for (let i = 1; i <= 800; i++) {
+    for (let i = 0; i < 800; i++) {
       const square = document.createElement("div");
       square.classList.add("square");
       board.appendChild(square);
