dd9391ea50a16676a83360376f5e6a99b136fc94
Akash-1212579 <dalbhanjanakash1@gmail.com>
2025-09-24 23:10:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //your JS code here. If required.
  const board = document.getElementById("board");
 
-    for (let i = 0; i < 800; i++) {
-      const box = document.createElement("div");
-      box.classList.add("square");
-      board.appendChild(box);
-		box.addEventListener("mouseover", () => {
-  box.style.backgroundColor = "tomato";
+    for (let i = 1; i <= 800; i++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      board.appendChild(square);
+		square.addEventListener("mouseover", () => {
+  square.style.backgroundColor = "tomato";
 });
-box.addEventListener("mouseout", () => {
-  box.style.backgroundColor = "rgb(29, 29, 29)";
+square.addEventListener("mouseout", () => {
+  square.style.backgroundColor = "rgb(29, 29, 29)";
 });
     }
