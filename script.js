f8395f86a5a18d69437a4bfa066f5e597adb3e07
Akash-1212579 <dalbhanjanakash1@gmail.com>
2025-09-24 23:03:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,12 @@
 
     for (let i = 0; i < 800; i++) {
       const box = document.createElement("div");
-      box.classList.add("box");
+      box.classList.add("square");
       board.appendChild(box);
+		box.addEventListener("mouseover", () => {
+  box.style.backgroundColor = "tomato";
+});
+box.addEventListener("mouseout", () => {
+  box.style.backgroundColor = "rgb(29, 29, 29)";
+});
     }
